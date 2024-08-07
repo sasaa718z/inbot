3a9d67a704fdd5eed4c8be71b7e7603ba0170edd
sasaa718z <arranatashya@gmail.com>
2024-08-07 13:21:41 +0700

    Add files via upload

---
+++ owner-dan-menu.js
@@ -0,0 +1,86 @@
+const fs = require('fs')
+
+global.namabot = "BOTZ-BY Anisa"
+global.namaowner = "Anisa Shop"
+global.footer_text = "© " + namabot
+global.pp_bot = fs.readFileSync("./image/foto.jpg")
+global.qris = fs.readFileSync("./image/qris.jpg")
+global.owner = ['6285794161086']
+global.pairingNumber = "6283899717120"
+global.sessionName = 'session'
+global.prefa = ['-_-']
+global.caption_pay = `Qris All Pay
+Ovo
+Dana
+Gopay
+
+Mau ganti payment? ketik .setpay
+`
+//menu bot rapihin sendiri ya, belajar lah jadi anak mandiri.
+module.exports.helpMenu = (pushname) =>{
+  return `Halo ${pushname}
+
+*「 BOTZ-BY Anisa >.< 」*
+
+\`\`\`» /owner
+» /addsewa
+» /delsewa
+» /ceksewa
+» /listsewa
+» /pay
+» /setpay
+» /list
+» /addlist
+» /updatelist
+» /renamelist
+» /dellist
+» /jeda
+» /tambah
+» /kurang
+» /kali
+» /bagi
+» /setproses
+» /changeproses
+» /delsetproses
+» /setdone
+» /changedone
+» /delsetdone
+» /proses
+» /done
+» /welcome
+» /goodbye
+» /setwelcome
+» /changewelcome
+» /delsetwelcome
+» /setleft
+» /changeleft
+» /delsetleft
+» /antiwame
+» /antiwame2
+» /antilink
+» /antilink2
+» /open
+» /close
+» /hidetag
+» /add
+» /kick
+» /stiker
+» /ffid
+» /mlid
+» /setppgc
+» /setnamegc
+» /setdesgc
+» /linkgc
+» /resetlinkgc
+» /promote
+» /demote
+» /setbot
+» /updatesetbot
+» /delsetbot
+» /bot
+\`\`\`
+
+📝 *NOTE*: 
+Ingin sewa bot? Ketik Owner
+`
+}
